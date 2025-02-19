c8265c3bcd2da8ec6edb994cc350024d9212f635
Roberto Cavalcanti <rbcer10@gmail.com>
2025-02-19 11:16:31 -0300

    analise rarefacao com vegan

---
+++ vegan-rarefy-example.R
@@ -0,0 +1,10 @@
+#Example script for rarefy function in vegan package
+#https://rdrr.io/rforge/vegan/man/rarefy.html
+library(vegan)
+data(BCI)
+S <- specnumber(BCI) # observed number of species
+(raremax <- min(rowSums(BCI)))
+Srare <- rarefy(BCI, raremax)
+plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+abline(0, 1)
+rarecurve(BCI, step = 20, sample = raremax, col = "blue", cex = 0.6)
